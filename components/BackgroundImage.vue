7e9e391a2108e0e9b5019efe57389379d8f9ade2
Emma Stotz <54609372+emmastotz@users.noreply.github.com>
2021-12-06 15:50:52 -0500

    feat: delete unnecessary components

--- components/BackgroundImage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <img
-    class="object-cover w-full h-auto"
-    src="@/assets/images/replacethespace.png"
-    alt="Replace the Space Logo"
-  >
-</template>
--- components/TheHeader.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="bg-gradient-to-br from-red-600 to-red-900 h-1/4 flex items-center justify-center">
-    <NuxtLink class="uppercase bg-red-100 text-red-600 font-semibold tracking-widest rounded-md px-4 shadow hover:shadow-lg hover:bg-white" to="/play">
-      Play
-    </NuxtLink>
-  </div>
-</template>
