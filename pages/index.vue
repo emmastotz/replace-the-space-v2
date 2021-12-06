4a5c2e99df42b388479dbeb36831b4e5d14b67c2
Emma Stotz <54609372+emmastotz@users.noreply.github.com>
2021-12-06 15:51:07 -0500

    feat: inline "components"

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="bg-gradient-to-tl from-blue-900 via-blue-700 to-blue-400 h-screen">
-    <TheHeader />
-    <div class="p-12">
-      <BackgroundImage />
+  <div class="bg-gradient-to-tl from-blue-900 via-blue-700 to-blue-400 h-screen overflow-hidden">
+    <div class="bg-gradient-to-tr from-red-600 to-red-900 h-1/4 flex items-center justify-center">
+      <NuxtLink class="uppercase bg-red-100 text-red-600 font-semibold tracking-widest rounded-md px-4 shadow hover:shadow-lg hover:bg-white" to="/play">
+        Play
+      </NuxtLink>
+    </div>
+    <div class="flex px-12 justify-center">
+      <img
+        class="object-cover lg:max-w-8xl"
+        src="@/assets/images/replacethespace.png"
+        alt="Replace the Space Logo"
+      >
     </div>
   </div>
 </template>
-
-<script>
-import BackgroundImage from '@/components/BackgroundImage.vue'
-import TheHeader from '@/components/TheHeader.vue'
-export default { components: { BackgroundImage, TheHeader } }
-</script>
