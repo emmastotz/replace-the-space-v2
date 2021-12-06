1118874027325cbef2c9d20af130df5ca882a687
Emma Stotz <54609372+emmastotz@users.noreply.github.com>
2021-12-06 15:16:28 -0500

    feat: styling

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-red-600 border-b-2 border-white h-1/4">
-    <span class="text-white uppercase">
-      Press play to begin
-    </span>
+  <div class="bg-gradient-to-br from-red-600 to-red-900 h-1/4 flex items-center justify-center">
+    <NuxtLink class="uppercase bg-red-100 text-red-600 font-semibold tracking-widest rounded-md px-4 shadow hover:shadow-lg hover:bg-white" to="/play">
+      Play
+    </NuxtLink>
   </div>
 </template>
