64b8f80977418d6c43c9109affa1567cf5fe4bac
Emma Stotz <54609372+emmastotz@users.noreply.github.com>
2021-12-07 11:09:18 -0500

    fix: language preference

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="bg-gradient-to-tl from-blue-900 via-blue-700 to-blue-400 h-screen overflow-hidden">
-    <div class="bg-gradient-to-tr from-red-600 to-red-900 flex items-center justify-center py-16">
-      <button v-if="!isPlaying" class="uppercase bg-red-100 text-red-600 font-semibold tracking-widest text-lg rounded-md px-5 py-1 shadow hover:shadow-lg hover:bg-white" @click="show">
+    <div class="bg-gradient-to-tr from-red-600 to-red-900 flex items-center justify-center py-16 w-full">
+      <button v-if="!isPlaying" class="uppercase bg-red-100 text-red-600 font-semibold tracking-widest text-lg rounded-md px-5 py-1 shadow hover:shadow-lg hover:bg-white" @click="play">
         Play
       </button>
       <div v-else class="grid gap-6">
@@ -12,7 +12,7 @@
         </div>
         <StatisticsDisplay
           :stats="stats"
-          :incorrect-guesses="incorrectGuesses"
+          :strikes="strikes"
           :guesses-left="guessesLeft"
         />
       </div>
@@ -31,6 +31,12 @@
 import BlankSpaces from '@/components/BlankSpaces.vue'
 import StatisticsDisplay from '@/components/StatisticsDisplay.vue'
 
+// const guessesLeft = 5
+
+const allowedStrikes = 3
+
+const defaultStrikes = new Array(allowedStrikes).fill({ icon: '⚪', guess: '' })
+
 export default {
   components: { BlankSpaces, StatisticsDisplay },
   data () {
@@ -41,6 +47,7 @@ export default {
       randomWord: {
         word: '',
         wordLength: 0,
+        guessLength: 0,
         letters: []
       },
       userGuess: '',
@@ -49,9 +56,8 @@ export default {
         wins: 0,
         losses: 0
       },
-      guessesLeft: 5,
       lettersGuessed: [],
-      incorrectGuesses: [],
+      strikes: [...defaultStrikes],
       wordLibrary: ['galaxy', 'neptune', 'blackhole', 'moon', 'eclipse', 'asteroid', 'jupiter', 'astronaut', 'telescope', 'mercury', 'density', 'saturn', 'mars', 'sun', 'star', 'comet', 'venus', 'uranus', 'earth', 'space', 'pluto', 'astronomy', 'equinox', 'cosmos', 'constellation', 'gravity'],
       alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     }
@@ -60,18 +66,24 @@ export default {
     this.initialize()
   },
   mounted () {
-    window.addEventListener('keypress', (e) => {
-      const key = e.key.toLowerCase()
-      if (key.length === 1 && this.alphabet.includes(key) && !this.lettersGuessed.includes(key)) {
-        this.userGuess = key
-      }
-
-      this.guess(this.userGuess)
-    })
+    window.addEventListener('keypress', e => this.handleKeyPress(e))
+  },
+  destroyed () {
+    window.removeEventListener('keypress', e => this.handleKeyPress(e))
   },
   methods: {
-    show () {
+    play () {
       this.isPlaying = !this.isPlaying
+
+      this.initialize()
+
+      if (this.guessesLeft > 0 && this.randomWord.guessLength === 0) {
+        this.win()
+      }
+
+      if (this.guessesLeft === 0 && this.randomWord.guessLength > 0) {
+        this.lose()
+      }
     },
     initialize () {
       // reset random word
@@ -84,7 +96,7 @@ export default {
       this.lettersGuessed = []
 
       // reset incorrect guesses array
-      this.incorrectGuesses = []
+      this.strikes = []
 
       // reset guesses left
       this.guessesLeft = 5
@@ -92,6 +104,7 @@ export default {
     generateRandomWord () {
       this.randomWord.word = this.wordLibrary[Math.floor(Math.random() * this.wordLibrary.length)]
       this.randomWord.wordLength = this.randomWord.word.length
+      this.randomWord.guessLength = this.randomWord.word.length
       for (let i = 0; i < this.randomWord.wordLength; i++) {
         this.randomWord.letters.push({
           id: i,
@@ -100,32 +113,32 @@ export default {
         })
       }
     },
+    handleKeyPress (e) {
+      const key = e.key.toLowerCase()
+      if (key.length === 1 && this.alphabet.includes(key) && !this.lettersGuessed.includes(key)) {
+        this.userGuess = key
+      }
+
+      this.guess(this.userGuess)
+    },
     guess (guess) {
       this.lettersGuessed.push(guess)
 
-      for (let i = 0; i <= this.randomWord.wordLength; i++) {
+      for (let i = 0; i <= this.randomWord.guessLength; i++) {
         if (guess === this.randomWord.word[i]) {
           this.showLetter(i)
           this.isCorrect = true
         }
       }
 
-      this.randomWord.wordLength--
+      this.randomWord.guessLength--
 
-      if (!this.isCorrect && this.guessesLeft > 0 && !this.lettersGuessed.includes(guess)) {
+      if (!this.isCorrect && this.guessesLeft > 0) {
         this.guessesLeft--
-        this.incorrectGuesses.push(guess)
+        this.strikes.push(guess)
       }
 
       this.isCorrect = false
-
-      if (this.guessesLeft === 0 && this.randomWord.wordLength > 0) {
-        this.lose()
-      }
-
-      if (this.guessesLeft > 0 && this.randomWord.wordLength === 0) {
-        this.win()
-      }
     },
     showLetter (i) {
       this.randomWord.letters[i].isShowing = !this.randomWord.letters[i].isShowing
