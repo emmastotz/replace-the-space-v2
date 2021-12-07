9c0e83ebd41112912a9df17535005486529288bf
Emma Stotz <54609372+emmastotz@users.noreply.github.com>
2021-12-06 19:30:42 -0500

    feat: `<StatisticsDisplay>`

---
+++ components/StatisticsDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid text-white font-semibold uppercase">
+    <span>
+      Wins: {{ stats.wins }}
+    </span>
+    <span>
+      Losses: {{ stats.losses }}
+    </span>
+    <span>
+      Guesses Left: {{ stats.guessesLeft }}
+    </span>
+    <div v-if="lettersGuessed.length > 0">
+      Letters Guessed:
+      <span v-for="letter of lettersGuessed" :key="letter">
+        {{ letter }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    stats: {
+      type: Object,
+      required: true
+    },
+    lettersGuessed: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
