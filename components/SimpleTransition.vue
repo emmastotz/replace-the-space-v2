3d422b9f59ee7e0f55468fc0eda198b1ea6ffc58
Emma Stotz <54609372+emmastotz@users.noreply.github.com>
2021-12-12 11:53:12 -0500

    feat: `<SimpleTransition>`

---
+++ components/SimpleTransition.vue
@@ -0,0 +1,28 @@
+<template>
+  <Transition
+    :enter-active-class="activeClass"
+    :leave-active-class="activeClass"
+    :enter-class="enterLeaveToClass"
+    :leave-to-class="enterLeaveToClass"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </Transition>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    activeClass: {
+      type: String,
+      required: true
+    },
+    enterLeaveToClass: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
